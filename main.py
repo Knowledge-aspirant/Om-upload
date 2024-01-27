0b5b3ca5492ae0c749f5052d9366378a4c410da8
Knowledge-aspirant <89350725+Knowledge-aspirant@users.noreply.github.com>
2024-01-27 22:04:53 +0530

    Update main.py

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ import sys
 import re
 import os
 
-bot = Client("6920580702:AAEpdOmEHcEqP5yJKIMH3Zld5l60EfB21Po", api_id=22495153, api_hash="34096b38e2f6c4d47ffc593cda42e4cd")
+bot = Client("6920580702:AAEpdOmEHcEqP5yJKIMH3Zld5l60EfB21Po", api_id= 22495153, api_hash= "34096b38e2f6c4d47ffc593cda42e4cd")
 
 
 @bot.on_message(filters.command(["start"]) & filters.user(ADMINS))

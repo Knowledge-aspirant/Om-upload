53c87e6e8527939ab6f2b1d0637b5429fb36f573
Knowledge-aspirant <89350725+Knowledge-aspirant@users.noreply.github.com>
2024-01-27 21:53:45 +0530

    Update main.py

--- main.py
+++ main.py
@@ -22,7 +22,9 @@ import sys
 import re
 import os
 
-bot = Client("6920580702:AAEpdOmEHcEqP5yJKIMH3Zld5l60EfB21Po", api_id=22495153, api_hash=34096b38e2f6c4d47ffc593cda42e4cd, bot_token=6920580702:AAEpdOmEHcEqP5yJKIMH3Zld5l60EfB21Po, enable_synchronized_clocks=True)
+bot = Client("6920580702:AAEpdOmEHcEqP5yJKIMH3Zld5l60EfB21Po", api_id=22495153, api_hash="34096b38e2f6c4d47ffc593cda42e4cd")
+await bot.start()
+await bot.sync_clock()
 
 
 @bot.on_message(filters.command(["start"]) & filters.user(ADMINS))
@@ -209,8 +211,6 @@ async def account_login(bot: Client, m: Message):
     except Exception as e:
         await m.reply_text(e)
     await m.reply_text("🔰Done🔰")
-await bot.start()
-await bot.sync_clock()
-bot.run(main)
+
 
 bot.run()
